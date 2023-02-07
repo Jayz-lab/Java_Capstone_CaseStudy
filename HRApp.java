4afeb9cab9581870d5aeb48bcf1e426666c2dd5f
JJ Chang (he/him) <75114824+Jayz-lab@users.noreply.github.com>
2023-02-07 15:48:45 +1100

    Create HRApp.java

---
+++ HRApp.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package HRApp;
+
+/**
+ *
+ * @author JJ
+ */
+public class HRApp_Capstone_caseStudy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Test that it compiles, runs and prints the initial message
+        System.out.println("HRApp starts.");
+        
+        //Test creating and printing an instance of the Employee class from the main method.
+        Employee e1 = new Employee(2355, "JJ", 65000);
+        Employee e2 = new Employee(2356, "Cassie", 65000);
+        System.out.println("Employee:" + e1);
+        System.out.println("Employee:" + e2);
+    }
+    
+}
