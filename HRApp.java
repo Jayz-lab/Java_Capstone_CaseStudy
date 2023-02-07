d8938d7425883cc4dceee1fc3e0a6683f6a13287
JJ Chang (he/him) <75114824+Jayz-lab@users.noreply.github.com>
2023-02-07 20:07:14 +1100

    Update HRApp.java

--- HRApp.java
+++ HRApp.java
@@ -21,8 +21,24 @@ public class HRApp_Capstone_caseStudy {
         //Test creating and printing an instance of the Employee class from the main method.
         Employee e1 = new Employee(2355, "JJ", 65000);
         Employee e2 = new Employee(2356, "Cassie", 65000);
-        System.out.println("Employee:" + e1);
-        System.out.println("Employee:" + e2);
+        //System.out.println("Employee:" + e1);
+        //System.out.println("Employee:" + e2);
+        
+        //Instantiate an instance of Department class
+        Department dept = new Department("Education");
+        
+        dept.addEmp(e1);
+        dept.addEmp(e2);
+        dept.addEmp(new Employee(772, "Jake", 65000));
+
+        Employee[] emps = dept.getEmployees();
+
+        for (Employee emp : emps) {
+            System.out.println("Emp " + emp);
+        }
+        System.out.println("Total " + dept.getTotalSalary());
+        System.out.println("Average " + dept.getAverageSalary());
+        System.out.println("Emp " + dept.getEmployeeByID(2355));
     }
     
 }
